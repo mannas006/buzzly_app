c4dfab1d53e1916207aeef0fa9c104faf39acf72
Manas Dey <manasdey.iron006@gmail.com>
2024-09-15 00:15:52 +0530

    init

---
+++ android/app/src/main/kotlin/com/example/buzzly/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.buzzly
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('WebView Example'),
+      ),
+      body: const WebView(
+        initialUrl: 'https://www.example.com',  // Replace with your desired URL
+      ),
+    );
+  }
+}
